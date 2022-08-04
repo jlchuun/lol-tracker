c80806c12931b752f231415f399cda78cf4e27e6
Joshua <jlchuun@gmail.com>
2022-08-03 21:04:36 -0700

    Connected front to back Search submit uses backend for riot api calls

--- src/services/summoner.js
+++ src/services/summoner.js
@@ -3,6 +3,7 @@ const baseUrl = '/summoner'
 
 const getSummoner = async (summonerName) => {
     const response  = await axios.get(`${baseUrl}/${summonerName}`)
+    console.log(response.data)
     return response.data
 }
 
